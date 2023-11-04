8697eb2c29829c68ce21f7f9c92bcb21104c6896
Heon Hong <ghdgjs3027@gmail.com>
2023-11-04 13:57:17 +0900

    동적프록시 특정 로그만 남기는 패턴 만들기

--- advanced1/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ advanced1/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,7 @@ import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_proxy.ConfigAlone;
 import hello.proxy.config.v2_proxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_proxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -18,8 +19,9 @@ import org.springframework.context.annotation.Import;
 
 //@Import(ConcreteProxyConfig.class)
 //@Import({InterfaceProxyConfig.class, ConcreteProxyConfig.class})
-@Import(ConfigAlone.class)
+//@Import(ConfigAlone.class)
 //@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
