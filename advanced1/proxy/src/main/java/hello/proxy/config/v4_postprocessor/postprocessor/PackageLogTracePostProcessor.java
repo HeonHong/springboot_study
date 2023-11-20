f29da5211371d886004a9e9867d791c09499767e
Heon Hong <ghdgjs3027@gmail.com>
2023-11-21 00:59:01 +0900

    빈 후처리기 만들기.(수동 컴포넌트 등록, component scan 대상 bean)

--- advanced1/proxy/src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/PackageLogTracePostProcessor.java
+++ advanced1/proxy/src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/PackageLogTracePostProcessor.java
@@ -1,4 +1,4 @@
-package hello.proxy.config.v4_postprocessor;
+package hello.proxy.config.v4_postprocessor.postprocessor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.Advisor;
@@ -24,6 +24,7 @@ public class PackageLogTracePostProcessor implements BeanPostProcessor {
 
         //프록시 적용 대상여부 체크
         //프록시 적용대상이 아니면 원본을 그대로 진행
+        //이렇게 처리하지 않으면 무수히 많은 springboot 기본 Bean들이 넘어온다.
         String pacakgeName = bean.getClass().getPackageName();
         if(!pacakgeName.startsWith(basePackage)){
             return bean;
