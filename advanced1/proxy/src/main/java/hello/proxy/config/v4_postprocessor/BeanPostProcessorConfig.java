6410cea54448b6aa3a690f73ae27d36973300013
Heon Hong <ghdgjs3027@gmail.com>
2023-11-23 01:06:47 +0900

    AspectJExpressionPointcut 적용

--- advanced1/proxy/src/main/java/hello/proxy/config/v4_postprocessor/BeanPostProcessorConfig.java
+++ advanced1/proxy/src/main/java/hello/proxy/config/v4_postprocessor/BeanPostProcessorConfig.java
@@ -34,4 +34,6 @@ public class BeanPostProcessorConfig {
 
         return new DefaultPointcutAdvisor(pointcut, advice);
     }
+
+
 }
--- advanced1/proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ advanced1/proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +16,41 @@ import org.springframework.context.annotation.Import;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-    @Bean
+//    @Bean
     public Advisor getAdvisor1(LogTrace logTrace){
         //pointcut
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*","order*","save*");
+
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    //AspectJExpressionPointcut
+//    @Bean
+    public Advisor getAdvisor2(LogTrace logTrace){
+        //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        //첫 ..은 app 하위 모든 패키지/ *은 모든 메서드/뒤 ..은 파라미터에 상관x
+        //아래 위치에 있어야 poincut 적용됨
+        pointcut.setExpression("execution(* hello.proxy.app..*(..))");
+
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    //AspectJExpressionPointcut
+    @Bean
+    public Advisor getAdvisor3(LogTrace logTrace){
+        //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        //첫 ..은 app 하위 모든 패키지/ *은 모든 메서드/뒤 ..은 파라미터에 상관x
+        //아래 위치에 있어야 poincut 적용됨
+        pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
 
         //advice
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
