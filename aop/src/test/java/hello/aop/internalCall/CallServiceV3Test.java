0090e98a8c90541c6a594bf760d294ea845946cc
Heon Hong <ghdgjs3027@gmail.com>
2024-01-10 00:25:26 +0900

    내부호출 문제 구조변경 해결 방법

---
+++ aop/src/test/java/hello/aop/internalCall/CallServiceV3Test.java
@@ -0,0 +1,28 @@
+package hello.aop.internalCall;
+
+import hello.aop.internalCall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV3Test {
+
+    @Autowired
+    CallServiceV3 callService;//프록시가 컨테이너에 저장됨.
+
+    @Test
+    void external() {
+//        log.info("target={}", callServiceV0.getClass());
+        callService.external();
+    }
+
+    @Test
+    void internal() {
+        internal();
+    }
+}
